80502698449f9b5108bcf24cdc30cdf9823995b2
Akash Ghosh <akashleo2009@gmail.com>
2024-07-23 23:57:23 +0530

    hero section update

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import "./pages.css";
 
 export default function Hero() {
   return (
-   <div className="hero-container">
-      <h1 className="hero-title">Your Name</h1>
-      <p className="hero-subtitle">A Brief Description or Tagline</p>
-      <a href="#projects" className="hero-button">
-        View My Work
-      </a>
+    <div className="hero-container flex flex-row justify-between">
+      <div className="profile-image"></div>
+      <div>
+        <h1 className="hero-title">Your Name</h1>
+        <p className="hero-subtitle">A Brief Description or Tagline</p>
+      </div>
     </div>
   );
 }
