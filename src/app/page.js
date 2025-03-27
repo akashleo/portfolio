14ed97c166ffae51380277fad79f74ae3bd1cafc
Akash Ghosh <akashleo2009@gmail.com>
2025-03-27 15:16:28 +0530

    navbar remove

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Hero />
         <Skills />
